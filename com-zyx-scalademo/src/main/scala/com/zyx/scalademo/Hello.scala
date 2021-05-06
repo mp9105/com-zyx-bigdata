b434d82ff6146cf8ee860737f134b265c5a53d72
zyx <zhangyaxi@fuyuanhk.com>
2021-05-06 11:09:50 +0800

    添加Scala环境及入门案例

---
+++ com-zyx-scalademo/src/main/scala/com/zyx/scalademo/Hello.scala
@@ -0,0 +1,16 @@
+package com.zyx.scalademo
+
+/**
+ * @author Yaxi.Zhang
+ * @since 2021/5/6 11:05
+ * desc: Hello world
+ */
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+    println("Hello World")
+    println("Hello World")
+    println("Hello World")
+    println("Hello World")
+  }
+}
